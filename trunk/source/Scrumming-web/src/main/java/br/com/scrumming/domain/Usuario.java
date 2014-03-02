932055f3cc18f7fd0df1decd236146bf09852fbb
esdras <servoesdras@gmail.com>
2014-03-02 18:30:45 -0300

    Mapeamento da entidade Usuario "close esdrasgit#1"
    Signed-off-by: esdras <servoesdras@gmail.com>

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/domain/Usuario.java
@@ -0,0 +1,117 @@
+package br.com.scrumming.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.NotBlank;
+import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.scrumming.core.infra.repositorio.HibernateTypes;
+import br.com.scrumming.core.infra.repositorio.ObjetoPersistente;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario extends ObjetoPersistente<Integer> {
+
+	/**
+	 * Serial Version
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="PK_usuario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	
+	@Column(name="nome", columnDefinition = "varchar(50)")
+	@NotBlank
+	private String nome;
+	
+	@Column(name="email", columnDefinition = "varchar(50)")
+	private String email;
+	
+	@Column(name="login", columnDefinition="varchar(30)")
+	private String login;
+
+	@Column(name= "senha", columnDefinition = "varchar(32)")
+	private String senha;
+
+	@Type(type = HibernateTypes.JODA_DATE_TIME)
+	@Column(name="data_cadastro")
+	@NotNull
+	private DateTime dataCadastro;
+	
+	@Column(name="is_ativo", columnDefinition= "bit" ,length = 1)
+	private boolean ativo;
+	
+	@Override
+	@JsonIgnore
+	public Integer getChave() {
+		return this.codigo;
+	}
+
+	/*getters and setters*/
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public DateTime getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(DateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+}
