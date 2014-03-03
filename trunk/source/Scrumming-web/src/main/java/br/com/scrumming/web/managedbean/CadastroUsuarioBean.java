9b6745c85520fc00828fda90947a1e3fc6589072
esdras <servoesdras@gmail.com>
2014-03-03 17:19:25 -0300

    close #2 Criar classe que representa o repositório de acesso aos dados da entidade Usuario.

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/managedbean/CadastroUsuarioBean.java
@@ -0,0 +1,10 @@
+package br.com.scrumming.web.managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CadastroUsuarioBean {
+
+}
