35a8c409f9408398bbe23972bd9b5f6e3f1c0686
danielvdantas <danielvdantas@hotmail.com>
2014-03-04 09:57:29 -0300

    Correção no JQuery

--- trunk/source/Scrumming-web/src/main/webapp/resources/js/plugins/flot/jquery.flot.js
+++ trunk/source/Scrumming-web/src/main/webapp/resources/js/plugins/flot/jquery.flot.js
@@ -1556,9 +1556,9 @@
             for (var j = 0; j < axes.length; ++j) {
                 var axis = axes[j], box = axis.box,
                     t = axis.tickLength, x, y, xoff, yoff;
-                if (!axis.show || axis.ticks.length == 0)
-                    continue
-                
+                if (!axis.show || axis.ticks.length == 0){
+                    continue;
+                }
                 ctx.strokeStyle = axis.options.tickColor || $.color.parse(axis.options.color).scale('a', 0.22).toString();
                 ctx.lineWidth = 1;
 
