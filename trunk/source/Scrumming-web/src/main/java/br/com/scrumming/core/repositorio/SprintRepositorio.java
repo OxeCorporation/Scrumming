2751e6e9a9be17b07ed636acc9283d196ca5e292
danielvdantas <danielvdantas@hotmail.com>
2014-03-04 09:53:36 -0300

    Criação do Repositorio Sprint

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/repositorio/SprintRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.scrumming.core.repositorio;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.scrumming.core.infra.repositorio.AbstractRepositorio;
+import br.com.scrumming.domain.Usuario;
+
+@Repository
+public class SprintRepositorio extends AbstractRepositorio<Usuario, Integer> {
+
+}
