d9bc6dff770b76b1d52cc3d100dbead04670dbbd
esdras <servoesdras@gmail.com>
2014-03-07 07:22:32 -0300

    Classes relacionadas ao Usuario
    Signed-off-by: esdras <servoesdras@gmail.com>

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/service/ContatoService.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/service/ContatoService.java
@@ -2,42 +2,44 @@ package br.com.scrumming.core.service;
 
 import java.util.Collection;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.scrumming.core.manager.interfaces.IContatoManager;
 import br.com.scrumming.domain.Contato;
 
-@Controller
+@RestController
 @RequestMapping("/contato")
 public class ContatoService {
 
-	@Autowired
-	private IContatoManager contatoManager;
+    @Autowired
+    private IContatoManager contatoManager;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody Collection<Contato> findAll() {
-		return this.contatoManager.findAll();
-	}
-		@RequestMapping(method = RequestMethod.GET, value = "/{nome}")
-		public @ResponseBody Collection<Contato> consultarContato(@PathVariable String nome){
-			if(nome != null && !nome.isEmpty()){
-				return contatoManager.consultarPorCampo("nome", nome);
-			}else{
-				return this.contatoManager.findAll();
-			}
-		}
-	
-	/* gettters and setters */
-	public IContatoManager getContatoManager() {
-		return contatoManager;
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Contato> findAll() {
+        return this.contatoManager.findAll();
+    }
 
-	public void setContatoManager(IContatoManager contatoManager) {
-		this.contatoManager = contatoManager;
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/{nome}")
+    public Collection<Contato> consultarContato(@PathVariable String nome) {
+
+        if (StringUtils.isNotBlank(nome)) {
+            return contatoManager.consultarPorCampo("nome", nome);
+        } else {
+            return this.contatoManager.findAll();
+        }
+    }
+
+    /* gettters and setters */
+    public IContatoManager getContatoManager() {
+        return contatoManager;
+    }
+
+    public void setContatoManager(IContatoManager contatoManager) {
+        this.contatoManager = contatoManager;
+    }
 }
---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/service/UsuarioService.java
@@ -0,0 +1,56 @@
+package br.com.scrumming.core.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.scrumming.core.manager.interfaces.IUsuarioManager;
+import br.com.scrumming.domain.Usuario;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioService {
+
+    @Autowired
+    private IUsuarioManager usuarioManager;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Usuario> listarTodos() {
+        return new ArrayList<Usuario>(usuarioManager.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{nome}")
+    public List<Usuario> consultarPorNome(@PathVariable String nome) {
+
+        List<Usuario> retorno;
+
+        if (StringUtils.isNotBlank(nome)) {
+            retorno = new ArrayList<Usuario>(usuarioManager.consultarPorNome(nome));
+        } else {
+            retorno = new ArrayList<Usuario>(usuarioManager.findAll());
+        }
+        return retorno;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{usuario}")
+    public void salvar(@PathVariable @Valid Usuario usuario) {
+        this.usuarioManager.insertOrUpdate(usuario);
+    }
+
+    /* getters and setters */
+    public IUsuarioManager getUsuarioManager() {
+        return usuarioManager;
+    }
+
+    public void setUsuarioManager(IUsuarioManager usuarioManager) {
+        this.usuarioManager = usuarioManager;
+    }
+}
