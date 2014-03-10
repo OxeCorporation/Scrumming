1db3e946bf6fec08c2c9190cf32cbff3c825b460
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-03-10 14:19:04 -0300

    implementação do metodo add usuario ao team

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/implementations/TeamManager.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/implementations/TeamManager.java
@@ -9,6 +9,7 @@ import br.com.scrumming.core.infra.manager.AbstractManager;
 import br.com.scrumming.core.infra.repositorio.AbstractRepositorio;
 import br.com.scrumming.core.manager.interfaces.ITeamManager;
 import br.com.scrumming.core.repositorio.TeamRepositorio;
+import br.com.scrumming.domain.Projeto;
 import br.com.scrumming.domain.Team;
 import br.com.scrumming.domain.Usuario;
 
@@ -34,6 +35,36 @@ public class TeamManager extends AbstractManager<Team, Integer> implements
 	public List<Usuario> consultarUsuarioPorProjeto(Integer usuarioID) {
 		return teamRepositorio.consultarUsuarioPorProjeto(usuarioID);
 	}
+	
+	public void associarTeamProjeto(Projeto projetoPersistido, List<Team> team) {
+		
+		for (Team item : team) {
+			
+			Team teamProjeto = new Team();
+			
+			teamProjeto.setEmpresa(item.getEmpresa());
+			teamProjeto.setProjeto(item.getProjeto());
+			teamProjeto.setUsuario(item.getUsuario());
+			teamProjeto.setPerfilUsuario(item.getPerfilUsuario());
+			teamProjeto.setAtivo(true);
+			
+			insertOrUpdate(teamProjeto);
+			
+		}
+	}
+
+
+	/*@Override
+	public List<Usuario> consultarUsuarioPorEmpresa(Integer empresaID) {
+		return null;// teamRepositorio.consultarUsuarioPorEmpresa(empresaID);
+	}*/
+	
+/*	@Override
+	public List<Usuario> addTimeProjeto(List<Usuario> usuarioEmpresa, Integer empresaID){
+		usuarioEmpresa=consultarUsuarioPorEmpresa(empresaID);
+		return teamRepositorio.consultarUsuarioPorProjeto(empresaID);
+	}*/
+
 
 	/* getters and setters */
 	public TeamRepositorio getTeamRepositorio() {
--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/ITeamManager.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/ITeamManager.java
@@ -3,9 +3,12 @@ package br.com.scrumming.core.manager.interfaces;
 import java.util.List;
 
 import br.com.scrumming.core.infra.manager.IManager;
+import br.com.scrumming.domain.Projeto;
 import br.com.scrumming.domain.Team;
 import br.com.scrumming.domain.Usuario;
 
 public interface ITeamManager extends IManager<Team, Integer> {
+	
+	public void associarTeamProjeto(Projeto projetoPersistido, List<Team> team);
 	List<Usuario> consultarUsuarioPorProjeto(Integer usuarioID); 
 }
