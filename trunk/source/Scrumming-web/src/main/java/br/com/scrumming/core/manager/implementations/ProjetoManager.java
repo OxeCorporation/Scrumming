d07c77f4ad27a750f84d8f0d520250102da9d449
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-03-10 14:15:59 -0300

    implementação do metodo add um usuario ao team

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/implementations/ProjetoManager.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/implementations/ProjetoManager.java
@@ -1,14 +1,20 @@
 package br.com.scrumming.core.manager.implementations;
 
 import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import br.com.scrumming.core.infra.manager.AbstractManager;
 import br.com.scrumming.core.infra.repositorio.AbstractRepositorio;
 import br.com.scrumming.core.manager.interfaces.IProjetoManager;
+import br.com.scrumming.core.manager.interfaces.ITeamManager;
 import br.com.scrumming.core.repositorio.ProjetoRepositorio;
 import br.com.scrumming.domain.Projeto;
+import br.com.scrumming.domain.Team;
+import br.com.scrumming.domain.Usuario;
 
 @Service
 public class ProjetoManager extends AbstractManager<Projeto, Integer> implements IProjetoManager {
@@ -20,11 +26,33 @@ public class ProjetoManager extends AbstractManager<Projeto, Integer> implements
 
     @Autowired
     private ProjetoRepositorio projetoRepositorio;
+    
+	@Autowired
+	private ITeamManager iTeamManage;
+
 
     @Override
     public AbstractRepositorio<Projeto, Integer> getRepositorio() {
         return this.projetoRepositorio;
     }
+
+	@Override
+	public void salvarProjeto(Projeto projeto, List<Team> team, List<Usuario> usuarioEmpresa) {
+
+		// Persiste o objeto Projeto e retorna a chave.
+		Integer projetoID = insertOrUpdate(projeto);
+		
+		// Caso sera inserido ou alterado a Sprint
+		if (projetoID != null) {
+			
+			// Busca o objeto persistido pela chave.
+			Projeto projetoPersistido = findByKey(projetoID);
+			
+			if (CollectionUtils.isNotEmpty(team)) {
+				iTeamManage.associarTeamProjeto(projetoPersistido, team);
+			}
+		}
+	}
 
     @Override
     public List<Projeto> consultarPorNome(String nome) {
--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/IProjetoManager.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/IProjetoManager.java
@@ -1,12 +1,17 @@
 package br.com.scrumming.core.manager.interfaces;
 
 import java.util.List;
+
 import org.joda.time.DateTime;
+
 import br.com.scrumming.core.infra.manager.IManager;
 import br.com.scrumming.domain.Projeto;
+import br.com.scrumming.domain.Team;
+import br.com.scrumming.domain.Usuario;
 
 public interface IProjetoManager extends IManager<Projeto, Integer> {
 
+	void salvarProjeto(Projeto sprint, List<Team> Team, List<Usuario> usuario);
     List<Projeto> consultarPorNome(String nome);
     List<Projeto> consultarPorPeriodo(DateTime dataInicio, DateTime dataFim);
 }
