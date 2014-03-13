2ccf00a55fa398b0aabe9d3ac3c5b7667b0761b3
naftalisabino <naftalijsabino@gmail.com>
2014-03-13 11:37:14 -0300

    Criação do Metodo CadastroItemBacklogBean.

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/managedbean/CadastroItemBacklogBean.java
@@ -0,0 +1,62 @@
+package br.com.scrumming.web.managedbean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.scrumming.domain.ItemBacklog;
+import br.com.scrumming.web.clientService.ItemBacklogClientService;
+
+@ManagedBean
+@ViewScoped
+public class CadastroItemBacklogBean extends AbstractBean {
+
+    private List<ItemBacklog> itens;
+    private ItemBacklog itemBacklog;
+    private ItemBacklogClientService clienteService;
+
+    @Override
+    public void inicializar() {
+        itens = new ArrayList<ItemBacklog>();
+        clienteService = new ItemBacklogClientService();
+        itemBacklog = new ItemBacklog();
+    }
+
+    
+	public String novo() {
+    	clienteService.salvarItemBacklog(itemBacklog);
+    	return "";
+    }
+
+    public String alterar() {
+       	clienteService.salvarItemBacklog(itemBacklog);
+        return "";
+
+    }
+
+    public String excluir() {
+       	clienteService.cancelarItemBacklog(itemBacklog);
+        return "";
+    }
+
+    /* getters and setters */
+    public List<ItemBacklog> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemBacklog> itens) {
+		this.itens = itens;
+	}
+
+	public ItemBacklog getItemBacklog() {
+		return itemBacklog;
+	}
+
+	public void setItemBacklog(ItemBacklog itemBacklog) {
+		this.itemBacklog = itemBacklog;
+	}
+
+    
+}
