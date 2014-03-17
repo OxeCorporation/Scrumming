e1e10b937e9208e0691e2be7dbbe979fd4ef9f2c
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-03-17 14:13:24 -0300

    ajustes em consultarUsuarioPorProjeto

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/repositorio/TeamRepositorio.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/repositorio/TeamRepositorio.java
@@ -4,12 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.expression.spel.ast.Projection;
 import org.springframework.stereotype.Repository;
 
 import br.com.scrumming.core.infra.repositorio.AbstractRepositorio;
+import br.com.scrumming.domain.Projeto;
 import br.com.scrumming.domain.Team;
 import br.com.scrumming.domain.Usuario;
 
@@ -22,11 +24,17 @@ public class TeamRepositorio extends AbstractRepositorio<Team, Integer> {
 	 * @return coleção de usuários
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Usuario> consultarUsuarioPorProjeto(Integer usuarioID) {
+	public List<Usuario> consultarUsuarioPorProjeto(Integer projetoID) {
 		Criteria criteria = createCriteria();
-		criteria.createAlias("usuario", "usuario");
-		criteria.add(Restrictions.eq("usuario.codigo", usuarioID));
-		return Collections.checkedList(criteria.list(), Usuario.class);
+		criteria.createAlias("projeto", "projetoAlias");
+		//criteria.add(Restrictions.eq("usuario.codigo", usuarioID));
+		//return Collections.checkedList(criteria.list(), Usuario.class);
+        criteria.addOrder(Order.asc("usuarioAlias.nome"));
+        criteria.add(Restrictions.eq("projetoAlias.codigo", projetoID));
+        criteria.add(Restrictions.not(Restrictions.eq("isAtivo", true)));
+        criteria.setProjection(Projections.property("usuario"));
+        return Collections.checkedList(criteria.list(), Usuario.class);
+
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -54,5 +62,18 @@ public class TeamRepositorio extends AbstractRepositorio<Team, Integer> {
 		
 		return Collections.checkedList(criteria.list(), Team.class);
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Usuario> consultarUsuarioPorEmpresaForaDoProjeto(Projeto projeto) {
+        Criteria criteria = createCriteria();
+        criteria.createAlias("empresa", "empresaAlias");
+        criteria.createAlias("usuario", "usuarioAlias");
+        criteria.addOrder(Order.asc("usuarioAlias.nome"));
+        criteria.add(Restrictions.eq("empresaAlias.codigo", projeto.getEmpresa().getCodigo()));
+        criteria.add(Restrictions.not(Restrictions.eq("usuarioAlias.codigo", "usuario.codigo")));
+        criteria.setProjection(Projections.property("usuario"));
+        return Collections.checkedList(criteria.list(), Usuario.class);
+	}
+
 	
 }
