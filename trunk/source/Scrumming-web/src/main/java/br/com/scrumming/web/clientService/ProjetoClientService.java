dc22cb33a2ad176e4bc65011add0ece35475d946
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-03-21 01:13:14 -0300

    Ajustes na arquitetura da classe

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/clientService/ProjetoClientService.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/clientService/ProjetoClientService.java
@@ -3,52 +3,23 @@ package br.com.scrumming.web.clientService;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
 import br.com.scrumming.domain.Projeto;
+import br.com.scrumming.domain.ProjetoDTO;
+import br.com.scrumming.web.infra.AbstractClientService;
+import br.com.scrumming.web.infra.ConstantesService;
 
-public class ProjetoClientService {
+public class ProjetoClientService extends AbstractClientService {
 
-	public void salvarProjeto(Projeto projeto) {
-
-		RestTemplate restTemplate = new RestTemplate();
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
-
-		String url = "http://localhost:8080/Scrumming/service/projeto/{salvarProjeto}";
-
-		restTemplate.postForEntity(url, requestEntity, null, projeto);
-
+	public String salvarProjeto(ProjetoDTO projetoDTO) {
+		return getRestTemplate().postForObject(getURIService(ConstantesService.Projeto.URL_SALVAR_PROJETO), projetoDTO, String.class);
 	}
 
-	public List<Projeto> consultartodosProjetos() {
-
-		RestTemplate restTemplate = new RestTemplate();
-
-		String url = "http://localhost:8080/Scrumming/service/projeto/{allProjeto}";
-
-		ResponseEntity<Projeto[]> projetos = restTemplate.getForEntity(url,
-				Projeto[].class);
-
-		
-		return Arrays.asList(projetos.getBody());
-	}
-	
-	public void deletarProjeto(Projeto projeto) {
-
-		RestTemplate restTemplate = new RestTemplate();
-
-		String url = "http://localhost:8080/Scrumming/service/projeto/list/{projetoID}";
-
-		restTemplate.delete(url, projeto);
+	public List<Projeto> consultarProjetosPorEmpresa(Integer empresaID) {
+		return Arrays.asList(getRestTemplate().getForObject(ConstantesService.Projeto.URL_CONSLTAR_POR_EMPRESA, Projeto[].class, empresaID));
 	}
 
-	
+	public String deletarProjeto(Integer projetoID) {
+		return getRestTemplate().postForObject(getURIService(ConstantesService.Projeto.URL_DELETE_PROJETO), projetoID, String.class);
+	}
+
 }
