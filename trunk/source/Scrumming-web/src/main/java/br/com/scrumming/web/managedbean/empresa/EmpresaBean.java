ef441fa03b00a36ec8a8f55370a1b7f001b67eeb
FilipeDelCesare <filipe_kyrios@hotmail.com>
2014-04-04 00:01:48 -0300

    Criação da classe "EmpresaBean" e seu respectivo package.

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/managedbean/empresa/EmpresaBean.java
@@ -0,0 +1,40 @@
+package br.com.scrumming.web.managedbean.empresa;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.scrumming.core.infra.util.ConstantesMensagem;
+import br.com.scrumming.domain.Empresa;
+import br.com.scrumming.web.clientService.EmpresaClientService;
+import br.com.scrumming.web.infra.FacesMessageUtil;
+import br.com.scrumming.web.infra.FlashScoped;
+import br.com.scrumming.web.infra.bean.AbstractBean;
+
+@ManagedBean
+@ViewScoped
+public class EmpresaBean extends AbstractBean {
+
+	@FlashScoped
+	private Empresa empresa;
+	private EmpresaClientService empresaClientService;
+	
+	@Override
+    public void inicializar() {
+		empresa = new Empresa();
+		empresaClientService = new EmpresaClientService();
+	}
+	
+	public void salvarEmpresa() {
+		empresaClientService.salvarEmpresa(empresa);
+    	FacesMessageUtil.adicionarMensagemInfo(ConstantesMensagem.MENSAGEM_OPERACAO_SUCESSO);
+	}
+	
+	/* getters and setters */
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+}
