951f7de8efbf74b18c20748736486fcdbcb89aed
FilipeDelCesare <filipe_kyrios@hotmail.com>
2014-04-07 10:24:34 -0300

    Ajustes na implementação e interface do manager de empresa para suportar o salvamento de um usuario

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/IEmpresaManager.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/manager/interfaces/IEmpresaManager.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import br.com.scrumming.core.infra.manager.IManager;
 import br.com.scrumming.domain.Empresa;
+import br.com.scrumming.domain.Usuario;
 
 public interface IEmpresaManager extends IManager<Empresa, Integer> {
+	
+	void salvar(Empresa empresa, Usuario usuario);
 
 	List<Empresa> consultarPorNome(String nome);
 	
