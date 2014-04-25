b8df645c7db5f0db797295fcc6a46df55365e035
pabhen <pabhencs@gmail.com>
2014-04-24 23:45:16 -0300

    Criado o clientService para o reporte de horas

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/clientService/TarefaReporteClientService.java
@@ -0,0 +1,16 @@
+package br.com.scrumming.web.clientService;
+
+import br.com.scrumming.domain.TarefaReporte;
+import br.com.scrumming.web.infra.AbstractClientService;
+import br.com.scrumming.web.infra.ConstantesService;
+
+public class TarefaReporteClientService extends AbstractClientService {
+	
+	public void reportarHora(TarefaReporte tarefaReporte) {		
+		getRestTemplate().postForObject(
+				getURIService(ConstantesService.TarefaReporte.URL_REPORTAR_HORA),
+				tarefaReporte, void.class, tarefaReporte.getCodigo(), 
+				tarefaReporte.getUsuario().getCodigo());
+	}	
+	
+}
