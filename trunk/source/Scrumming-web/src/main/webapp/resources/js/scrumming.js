ec2b976012c62af077b8af9fc9ea77bd275f3795
naftalisabino <naftalijsabino@gmail.com>
2014-04-27 22:45:09 -0300

    Ajuste no slider do Story Point em itembacklog

--- trunk/source/Scrumming-web/src/main/webapp/resources/js/scrumming.js
+++ trunk/source/Scrumming-web/src/main/webapp/resources/js/scrumming.js
@@ -1,66 +1,33 @@
-$( "#slider" ).slider({
-    value: 0,
-    min: 0,
-    max: 9,
-    //range: "min",
-    step: 1
-})
-.each(function() {
-  
-  var valorOption = $("#slider").slider("value");
-  var valor = 0;
-  var valorString;
-  if(valorOption == 0){
-	  valor= 0;
-	  valorString= valor.toString();
-	  $("#outValueSP").value= valorString;
-	  //document.setElementById("myForm:totalCount").value=total;
-	  
-  }	else if(valorOption == 1){
-	  valor= 1;
-	  valorString= valor.toString();
-	  $("#outValueSP").value= valorString;
-	  
-  }	else if(valorOption == 2){
-	  valor= 2;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 3){
-	  valor= 1;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 4){
-	  valor= 5;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 5){
-	  valor= 8;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 6){
-	  valor= 13;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 7){
-	  valor= 20;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }	else if(valorOption == 8){
-	  valor= 40;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  } else if(valorOption == 9){
-	  valor= 100;
-	  valorString= valor.toString();
-	  $("#outValueSP").val(valorString);
-	  
-  }
-  
+function fib(n) {
+        if (n == 0) {
+            return n = 0;
+        }else if(n == 1){
+        	return n = 1;
+        }else if(n == 2){
+        	return n = 2;
+        }else if(n == 3){
+        	return n = 3;
+        }else if(n == 4){
+        	return n = 5;
+        }else if(n == 5){
+        	return n = 8;
+        }else if(n == 6){
+        	return n = 13;
+        }else if(n == 7){
+        	return n = 20;
+        }else if(n == 8){
+        	return n = 40;
+        }else if(n == 9){
+        	return n = 100;
+        }
+}
+$(function(){
+    $("#slider").slider({
+        value: 0,
+        min: 0,
+        max: 9,
+        change: function(event,ui){
+             $("#valor").html(fib(ui.value));        
+        }
+    });       
 });
