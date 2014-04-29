91f324e18c3d4970af2e224d7c6a108a4e4b5243
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-04-29 12:19:42 -0300

    retornando ao estado funcional de cadastro de projeto

--- trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/managedbean/projeto/ProjetoCadastroMB.java
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/web/managedbean/projeto/ProjetoCadastroMB.java
@@ -1,5 +1,6 @@
 package br.com.scrumming.web.managedbean.projeto;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -10,22 +11,14 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
-import org.primefaces.event.DashboardReorderEvent;
-import org.primefaces.model.DashboardColumn;
-import org.primefaces.model.DashboardModel;
-import org.primefaces.model.DefaultDashboardColumn;
-import org.primefaces.model.DefaultDashboardModel;
-
 import br.com.scrumming.core.infra.util.ConstantesMensagem;
 import br.com.scrumming.domain.Empresa;
 import br.com.scrumming.domain.Projeto;
 import br.com.scrumming.domain.ProjetoDTO;
 import br.com.scrumming.domain.Team;
 import br.com.scrumming.domain.Usuario;
-import br.com.scrumming.domain.UsuarioList;
 import br.com.scrumming.domain.enuns.PerfilUsuarioEnum;
 import br.com.scrumming.domain.enuns.SituacaoProjetoEnum;
-import br.com.scrumming.domain.enuns.SituacaoUsuarioList;
 import br.com.scrumming.web.clientService.ProjetoClientService;
 import br.com.scrumming.web.infra.FacesMessageUtil;
 import br.com.scrumming.web.infra.FlashScoped;
@@ -37,6 +30,7 @@ import br.com.scrumming.web.infra.bean.AbstractBean;
 public class ProjetoCadastroMB extends AbstractBean {
 
 	private static final long serialVersionUID = 1L;
+	
 	@FlashScoped
 	private Projeto projetoSelecionado;
 	@FlashScoped
@@ -46,7 +40,6 @@ public class ProjetoCadastroMB extends AbstractBean {
 	private List<Usuario> usuarioEmpresaNotProjeto;
 	@FlashScoped
 	private List<Team> teamProjeto;
-	private List<UsuarioList> usuariosNaLista;
 
 	@SuppressWarnings("unused")
 	private List<PerfilUsuarioEnum> todosPerfis;
@@ -63,10 +56,6 @@ public class ProjetoCadastroMB extends AbstractBean {
 	@FlashScoped
 	private String titulo;
 
-	private DashboardModel dashboardModel;
-	@FlashScoped
-	private List<UsuarioList> usuariosList;
-	private static final String PREFIX_TASK = "task_id_";
 
 	@Override
 	protected void inicializar() {
@@ -76,34 +65,16 @@ public class ProjetoCadastroMB extends AbstractBean {
 		if (teamProjeto == null) {
 			teamProjeto = new ArrayList<>();
 		}
-		if (usuariosNaLista == null) {
-			usuariosNaLista = new ArrayList<>();
-		}
 
 		if (projetoDTO == null) {
 			projetoDTO = new ProjetoDTO();
 		}
-		usuariosList = criarFaceTarefa();
 		projetoClientService = new ProjetoClientService();
 		
-
-		init();
 	}
 
 	@SuppressWarnings("static-access")
 	public String salvarProjeto() {
-		for (UsuarioList usuarioList : usuariosList) {
-			if (usuarioList.getSituacaoUsuarioList().equals(
-					SituacaoUsuarioList.DO_PROJETO)) {
-				Team team = new Team();
-				team.setEmpresa(empresa);
-				team.setUsuario(usuarioList.getUsuario());
-				team.setPerfilUsuario(PerfilUsuarioEnum.TEAM);
-				team.setProjeto(projetoSelecionado);
-				team.setAtivo(true);
-				teamProjeto.add(team);
-			}
-		}
 		String retorno = "";
 		projetoDTO.getProjeto().setEmpresa(empresa);
 		projetoDTO.getProjeto().setSituacaoProjeto(situacao.ATIVO);
@@ -114,73 +85,6 @@ public class ProjetoCadastroMB extends AbstractBean {
 				.adicionarMensagemInfo(ConstantesMensagem.MENSAGEM_OPERACAO_SUCESSO);
 		retorno = redirecionar(PaginasUtil.Projeto.PROJETO_PAGE);
 		return retorno;
-	}
-
-	private void init() {
-		dashboardModel = new DefaultDashboardModel();
-		DashboardColumn timeProj = new DefaultDashboardColumn();
-		DashboardColumn usuarioDisp = new DefaultDashboardColumn();
-
-		dashboardModel.addColumn(timeProj);
-		dashboardModel.addColumn(usuarioDisp);
-
-		for (UsuarioList tarefa : usuariosNaLista) {
-			DashboardColumn column = dashboardModel.getColumn(tarefa
-					.getSituacaoUsuarioList().ordinal());
-			column.addWidget(PREFIX_TASK + tarefa.getUsuario().getCodigo());
-		}
-	}
-
-	private List<UsuarioList> criarFaceTarefa() {
-		for (Usuario tarefa : usuarioEmpresaNotProjeto) {
-			UsuarioList usuarioList = new UsuarioList();
-			usuarioList.setUsuario(tarefa);
-			usuarioList.setSituacaoUsuarioList(SituacaoUsuarioList.NOT_PROJETO);
-			usuariosNaLista.add(usuarioList);
-
-		}
-		for (Team tarefa : teamProjeto) {
-			UsuarioList usuarioList = new UsuarioList();
-			usuarioList.setUsuario(tarefa.getUsuario());
-			usuarioList.setSituacaoUsuarioList(SituacaoUsuarioList.DO_PROJETO);
-			usuariosNaLista.add(usuarioList);
-
-		}
-
-		return usuariosNaLista;
-	}
-
-	public void handleReorder(DashboardReorderEvent event) {
-		String widgetId = event.getWidgetId();
-		int columnIndex = event.getColumnIndex();
-		UsuarioList task = findTarefa(Integer.valueOf(widgetId
-				.substring(PREFIX_TASK.length())));
-		task.setSituacaoUsuarioList((SituacaoUsuarioList.values()[columnIndex]));
-	}
-
-	private UsuarioList findTarefa(Integer id) {
-		for (UsuarioList tarefa : usuariosList) {
-			if (tarefa.getUsuario().getCodigo().equals(id)) {
-				return tarefa;
-			}
-		}
-		return null;
-	}
-
-	public DashboardModel getDashboardModel() {
-		return dashboardModel;
-	}
-
-	public void setDashboardModel(DashboardModel dashboardModel) {
-		this.dashboardModel = dashboardModel;
-	}
-
-	public List<UsuarioList> getTarefas() {
-		return usuariosList;
-	}
-
-	public void setTarefas(List<UsuarioList> tarefas) {
-		this.usuariosList = tarefas;
 	}
 
 	public String moveItemToTeam() {
@@ -369,21 +273,4 @@ public class ProjetoCadastroMB extends AbstractBean {
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
-	public List<UsuarioList> getUsuariosNaLista() {
-		return usuariosNaLista;
-	}
-
-	public void setUsuariosNaLista(List<UsuarioList> usuariosNaLista) {
-		this.usuariosNaLista = usuariosNaLista;
-	}
-
-	public List<UsuarioList> getUsuariosList() {
-		return usuariosList;
-	}
-
-	public void setUsuariosList(List<UsuarioList> usuariosList) {
-		this.usuariosList = usuariosList;
-	}
-
 }
