6f163d3fa64b740dd7a9681e309c4fa3335355eb
Daniel <danielvdantas@hotmail.com>
2014-05-06 11:00:02 -0300

    Mapeamento da tabela de Config e criação inicial do Enum que representará todas as configurações do sistema para a Empresa.

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/domain/Config.java
@@ -0,0 +1,100 @@
+package br.com.scrumming.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.NotBlank;
+import br.com.scrumming.core.infra.repositorio.ObjetoPersistente;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "Config")
+public class Config extends ObjetoPersistente<Integer> {
+
+    /**
+     * Serial Version
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "PK_config")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer codigo;
+    
+    @ManyToOne
+    @JoinColumn(name="FK_empresa", referencedColumnName="PK_empresa")
+    private Empresa empresa;
+
+    @Column(name = "nome_config", columnDefinition = "varchar(50)")
+    @NotBlank
+    private String nomeConfig;
+    
+    @Column(name = "perfil_owner", columnDefinition = "bit")
+    private boolean perfilOwner;
+    
+    @Column(name = "perfil_master", columnDefinition = "bit")
+    private boolean perfilMaster;
+    
+    @Column(name = "perfil_team", columnDefinition = "bit")
+    private boolean perfilTeam;
+    
+    /* getters and setters */
+    @Override
+    @JsonIgnore
+    public Integer getChave() {
+        return this.codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getNomeConfig() {
+		return nomeConfig;
+	}
+
+	public void setNomeConfig(String nomeConfig) {
+		this.nomeConfig = nomeConfig;
+	}
+
+	public boolean isPerfilOwner() {
+		return perfilOwner;
+	}
+
+	public void setPerfilOwner(boolean perfilOwner) {
+		this.perfilOwner = perfilOwner;
+	}
+
+	public boolean isPerfilMaster() {
+		return perfilMaster;
+	}
+
+	public void setPerfilMaster(boolean perfilMaster) {
+		this.perfilMaster = perfilMaster;
+	}
+
+	public boolean isPerfilTeam() {
+		return perfilTeam;
+	}
+
+	public void setPerfilTeam(boolean perfilTeam) {
+		this.perfilTeam = perfilTeam;
+	}
+}
---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/domain/enuns/ConfigEnum.java
@@ -0,0 +1,5 @@
+package br.com.scrumming.domain.enuns;
+
+public enum ConfigEnum {
+	NO_CONFIG, CADASTRO_PROJETO, CADASTRO_SPRINT, FECHAR_SPRINT;
+}
