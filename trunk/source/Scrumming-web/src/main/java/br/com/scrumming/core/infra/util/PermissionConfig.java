2f763d9b2494b519ed7086b6d303bec3779ba4e3
Daniel <danielvdantas@hotmail.com>
2014-05-07 17:07:11 -0300

    Criação da classe que fará a verificação de permissão.

---
+++ trunk/source/Scrumming-web/src/main/java/br/com/scrumming/core/infra/util/PermissionConfig.java
@@ -0,0 +1,13 @@
+package br.com.scrumming.core.infra.util;
+
+import br.com.scrumming.domain.Usuario;
+import br.com.scrumming.domain.enuns.ConfigEnum;
+
+public class PermissionConfig {
+
+	public static boolean verifyPermission(Usuario usuario, ConfigEnum configEnum) {
+		boolean allowed = false;
+		
+		return allowed;
+	}
+}
