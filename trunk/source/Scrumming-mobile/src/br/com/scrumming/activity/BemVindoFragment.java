c257a1879138f8312b8166d03acf5f47569de859
FilipeDelCesare <filipe_kyrios@hotmail.com>
2014-05-10 16:03:37 -0300

    Activity/Fragment da tela após o usuário logar. OBS: Tela de teste, depois deve ser renomeada para ser a visualzação do board.

---
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/activity/BemVindoFragment.java
@@ -0,0 +1,19 @@
+package br.com.scrumming.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import br.com.scrumming.R;
+
+public class BemVindoFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_login, container);
+		
+		return view;
+	}
+}
