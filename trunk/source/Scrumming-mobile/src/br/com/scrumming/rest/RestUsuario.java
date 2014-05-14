d8b97984d66f99ec81f416d2db9d99f3cda874a3
pabhen <pabhencs@gmail.com>
2014-05-14 02:17:47 -0300

    merge

--- trunk/source/Scrumming-mobile/src/br/com/scrumming/rest/RestUsuario.java
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/rest/RestUsuario.java
@@ -16,7 +16,7 @@ public class RestUsuario {
 	public static Usuario retorneUsuario(String login, String senha){
 		
 		//String domain = "scrumming-agilscrum.rhcloud.com";
-		String domain = "192.168.1.2:8080";
+		String domain = "192.168.0.101:8080";
 		final String url = "http://"+domain+"/Scrumming/service/usuario/login/{login}/{senha}";
 		Usuario usuario = RestFactory.getRestTemplate().postForObject(url, HttpEntity.EMPTY, Usuario.class,login,senha);
 		return usuario;
