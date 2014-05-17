515c20647fadb48aa6cf49e0338eebdd6bfc2790
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-05-17 17:33:00 -0300

    Validações na tela de login

--- trunk/source/Scrumming-mobile/src/br/com/scrumming/activity/LoginActivity.java
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/activity/LoginActivity.java
@@ -2,13 +2,14 @@ package br.com.scrumming.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 import br.com.scrumming.R;
 import br.com.scrumming.asynctask.AsyncTaskUsuario;
 import br.com.scrumming.domain.Usuario;
@@ -17,11 +18,14 @@ import br.com.scrumming.interfaces.InterfaceUsuario;
 public class LoginActivity extends Activity implements InterfaceUsuario {
 	
 	EditText textLogin, textSenha;
+	String titulo, mensagem, login, senha;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+
         
         textLogin = (EditText) findViewById(R.id.editTxtLogin);
 		textSenha = (EditText) findViewById(R.id.editTxtSenha);
@@ -32,21 +36,51 @@ public class LoginActivity extends Activity implements InterfaceUsuario {
     private OnClickListener btnLogarOnClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			String login = textLogin.getText().toString();
-			String senha = textSenha.getText().toString();
-			new AsyncTaskUsuario(LoginActivity.this).execute(login, senha);
+			login = textLogin.getText().toString();
+			senha = textSenha.getText().toString();
+			if (login!=null && !senha.equals("")){
+				new AsyncTaskUsuario(LoginActivity.this).execute(login, senha);
+			}else{
+				if(login.equals("")){
+					titulo="Informação";
+					mensagem = "Informe o login";
+				}
+				if (senha.equals("")) {
+					titulo="Informação";
+					mensagem = "Informe a senha";
+				}if(login.equals("") && senha.equals("")){
+					titulo="Informação";
+					mensagem = "Informe login e senha";
+				}
+				alert(titulo, mensagem);
+			}
 		}
 	};
+	
+	@SuppressWarnings("deprecation")
+	public void alert(String titulo, String mensagem){
+        AlertDialog alerta = new AlertDialog.Builder(this).create();
+        alerta.setTitle(titulo);
+        alerta.setMessage(mensagem);
+        alerta.setButton("OK", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+        alerta.show();
+	}
 	
 	@SuppressLint("ShowToast")
 	@Override
 	public void logarComUsuario(Usuario usuario) {
-		Toast.makeText(this, usuario.getEmail(), Toast.LENGTH_LONG);
-		Toast toast;
-		if (usuario.getCodigo() == null) {
-			toast = Toast.makeText(this, "Usuário não Existe", Toast.LENGTH_LONG);
-			toast.show();
+		if (usuario == null) {
+			titulo = "Infermação";
+			mensagem = "Login ou senha inválida";
+			alert(titulo, mensagem);
 		}else{
+			login ="";
+			senha = "";
 			Intent it = new Intent(this, PrincipalActivity.class);
 			it.putExtra("usuario", usuario);
 			startActivity(it);
