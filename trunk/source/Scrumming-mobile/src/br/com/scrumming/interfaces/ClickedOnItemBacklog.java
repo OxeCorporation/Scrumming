97c74b865cf513f248fc046e897fb03d61e227fb
FilipeDelCesare <filipe_kyrios@hotmail.com>
2014-05-17 19:33:47 -0300

    Criação da interface ClickedOnItemBacklog

---
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/interfaces/ClickedOnItemBacklog.java
@@ -0,0 +1,7 @@
+package br.com.scrumming.interfaces;
+
+import br.com.scrumming.domain.ItemBacklog;
+
+public interface ClickedOnItemBacklog {
+	void itemBacklogFoiClicada(ItemBacklog itemBacklog);
+}
