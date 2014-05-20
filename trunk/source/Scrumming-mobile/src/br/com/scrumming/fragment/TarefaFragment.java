e677661ce3f84b2caac10888b3d20a71d8987bda
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-05-20 14:37:44 -0300

    Fragment Tarefa criada

---
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/fragment/TarefaFragment.java
@@ -0,0 +1,92 @@
+package br.com.scrumming.fragment;
+
+import java.util.List;
+
+import android.os.AsyncTask;
+import android.os.AsyncTask.Status;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import br.com.scrumming.R;
+import br.com.scrumming.adapter.TarefaAdapter;
+import br.com.scrumming.domain.ItemBacklog;
+import br.com.scrumming.domain.Tarefa;
+import br.com.scrumming.domain.UsuarioEmpresa;
+import br.com.scrumming.rest.RestTarefa;
+
+public class TarefaFragment extends ListFragment {
+	
+	List<Tarefa> listaTarefa;
+	AsyncTaskTarefa taskTarefa;
+	ItemBacklog itemBacklog;
+	UsuarioEmpresa usuarioEmpresa;
+	Integer sprintID, usuarioID;
+	
+	public static TarefaFragment novaInstancia(ItemBacklog itemBacklog, UsuarioEmpresa usuarioEmpresa){
+		Bundle args = new Bundle();
+		args.putSerializable("itemBacklog", itemBacklog);
+		args.putSerializable("usuarioEmpresa", usuarioEmpresa);
+		
+		TarefaFragment bvf = new TarefaFragment();
+		bvf.setArguments(args);
+		return bvf;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setRetainInstance(true);
+
+		if (listaTarefa != null){
+			AtualizarListaDeTarefa();;
+
+		} else {
+			if (taskTarefa != null && taskTarefa.getStatus() == Status.RUNNING){
+				//mostrarProgress();
+
+			} else {
+				taskTarefa = new AsyncTaskTarefa();
+				taskTarefa.execute(itemBacklog.getCodigo());
+			}
+		}
+		
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View layout = inflater.inflate(R.layout.fragment_tarefa, container, false);
+		
+		//pega a sprint clicada no sprintFragment para listar os itensbacklog da sprint
+		itemBacklog = (ItemBacklog) getArguments().getSerializable("itemBacklog");
+		usuarioEmpresa = (UsuarioEmpresa)getArguments().getSerializable("usuarioEmpresa");
+		
+		return layout;
+	}
+	
+	private void AtualizarListaDeTarefa() {
+		TarefaAdapter adapter = new TarefaAdapter(getActivity(), listaTarefa);
+		setListAdapter(adapter);
+	}
+	
+	class AsyncTaskTarefa extends AsyncTask<Integer, Void, List<Tarefa>>{
+
+		@Override
+		protected List<Tarefa> doInBackground(Integer... params) {
+			return RestTarefa.retornarTarefa(params[0]);
+		}
+		
+		@Override
+		protected void onPostExecute(List<Tarefa> tarefas) {
+			super.onPostExecute(tarefas);
+			if(tarefas != null) {
+				listaTarefa = tarefas;
+				AtualizarListaDeTarefa();;
+			}
+		}
+
+	}
+
+}
