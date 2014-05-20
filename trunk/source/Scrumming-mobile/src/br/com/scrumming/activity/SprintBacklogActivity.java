6e671d0cf19cd3102e7887af52cee639d0ceac9f
SandroUnibratec <sandro2011fernandes@gmail.com>
2014-05-20 14:42:30 -0300

    alteração na activity para listagem de tarefas

--- trunk/source/Scrumming-mobile/src/br/com/scrumming/activity/SprintBacklogActivity.java
+++ trunk/source/Scrumming-mobile/src/br/com/scrumming/activity/SprintBacklogActivity.java
@@ -1,13 +1,16 @@
 package br.com.scrumming.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import br.com.scrumming.R;
+import br.com.scrumming.domain.ItemBacklog;
 import br.com.scrumming.domain.Sprint;
 import br.com.scrumming.domain.UsuarioEmpresa;
 import br.com.scrumming.fragment.SprintBacklogFragment;
+import br.com.scrumming.interfaces.ClickedOnItemBacklog;
 
-public class SprintBacklogActivity extends FragmentActivity{
+public class SprintBacklogActivity extends FragmentActivity implements ClickedOnItemBacklog{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,15 @@ public class SprintBacklogActivity extends FragmentActivity{
 			.add(R.id.master, sbf)
 			.commit();			
 	}
+	
+	@Override
+	public void itemBacklogFoiClicada(ItemBacklog itemBacklog, UsuarioEmpresa usuarioEmpresa) {
+		Intent it4 = new Intent(this, TarefaActivity.class);
+		it4.putExtra("itemBacklog", itemBacklog);
+		it4.putExtra("usuarioEmpresa", usuarioEmpresa);
+		startActivity(it4);
+		
+	}
+
 
 }
